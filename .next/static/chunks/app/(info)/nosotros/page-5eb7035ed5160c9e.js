de8b078595edf5db600037f4e15224183f8198ba
giancarlotiradoarana <giancarlotiradoarana@users.noreply.github.com>
2026-09-06 10:52:39 -0500

    Ignorar .next (build generado); mantener .env, dev.db y demas archivos

--- .next/static/chunks/app/(info)/nosotros/page-5eb7035ed5160c9e.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[766,2206,5232,5351,6843,7098],{29831:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,52619,23))}},e=>{e.O(0,[2619,8441,1255,7358],()=>e(e.s=29831)),_N_E=e.O()}]);
